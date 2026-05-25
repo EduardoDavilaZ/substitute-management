f2b3c3b26c4c8394c15356e0fbc32726ef179cf4
eduardo <edavilazevallos@gmail.com>
2026-05-25 22:02:12 +0200

    Fix absence history

--- src/app/controllers/AbsenceController.php
+++ src/app/controllers/AbsenceController.php
@@ -1,15 +1,28 @@
 <?php
-    final class AbsenceController extends Controller
-    {
+final class AbsenceController extends Controller
+{
 
-        public function getAbsencesDetail()
-        {
-            return json(['data' => (new Absence())->getAbsencesDetails()]);
-        }
-        public function getDataView(int $id)
-        {
-            $this->view = 'admin/modals/detail_substitution_modal';
-            $results = (new Absence())->getAbsencesDetailsById($id);
-            return ['data' => $results[0] ?? []];
-        }
+    public function getAbsencesDetail()
+    {
+        return json(['data' => (new Absence())->getAbsencesDetails()]);
     }
+    public function getDataView(int $id)
+    {
+        $this->view = 'admin/modals/detail_substitution_modal';
+        $results = (new Absence())->getAbsencesDetailsById($id);
+        return ['data' => $results[0] ?? []];
+    }
+
+    public function getAbsencesHistory()
+    {
+        return json(['data' => (new Absence())->getAbsencesHistory()]);
+    }
+
+    public function getAbsenceById(int $id = 0) : array
+    {
+        $this->view   = 'admin/modals/absence_detail_modal';
+        $this->layout = null;
+
+        return ['absence' => (new Absence())->getAbsencesDetailsById($id)];
+    }
+}
